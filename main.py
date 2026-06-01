3aeb89ee594f40f692eaa0ab73b1cc61a01073b5
Franklin Wang <50060084+franklinwangg@users.noreply.github.com>
2026-06-01 01:48:02 +0000

    changed allowed origins to everything to unblock cors error on frontend

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controllers.auth_controller import router as auth_router
 from controllers.resume_controller import router as resume_router
 from controllers.tailored_resume_controller import router as tailored_resume_router
 
 app = FastAPI(title="AutoShake API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(resume_router)
 app.include_router(tailored_resume_router)
